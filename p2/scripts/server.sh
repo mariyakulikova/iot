1ff1e4afdf53277262fd5ad7b9a9ef5a9805dd15
mkulikov <mkulikov@student.42berlin.de>
2026-07-28 13:10:22 +0200

    p2: update server setup script

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -14,9 +14,19 @@ cp /vagrant/confs/server.yaml "$CONFIG_DIRECTORY/config.yaml"
 
 chmod 0600 "$CONFIG_DIRECTORY/config.yaml"
 
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
 
-KUBECTL_ALIAS="alias k='sudo kubectl'"
+KUBECTL_ALIAS="alias k='kubectl'"
 VAGRANT_BASHRC="/home/vagrant/.bashrc"
 
 grep -qxF "$KUBECTL_ALIAS" "$VAGRANT_BASHRC" || echo "$KUBECTL_ALIAS" >> "$VAGRANT_BASHRC"
+
+timeout 180 bash -c '
+    until kubectl get nodes >/dev/null 2>&1; do
+        sleep 2
+    done
+'
+
+kubectl wait --for=condition=Ready node --all --timeout=180s
+
+kubectl apply -f /vagrant/confs/manifests/
